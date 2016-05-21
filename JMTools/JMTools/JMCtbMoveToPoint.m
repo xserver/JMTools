77795452eda1210ab6d8189b4f39de7696bba76f
积木.JM <xqlday@163.com>
2016-05-21 14:37:10 +0800

    更新转场动画

--- JMTools/JMTools/JMCtbMoveToPoint.m
+++ JMTools/JMTools/JMCtbMoveToPoint.m
@@ -12,22 +12,35 @@
 
 - (void)animateTransition:(id <UIViewControllerContextTransitioning>)transitionContext {
     
-        UIViewController *fromCtrl = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
-        UIView *fromView = fromCtrl.view;
-    
-    UIViewController *toCtrl = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
-    UIView *toView = toCtrl.view;
-    
+    UIView *fromView = [[transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey] view];
+    UIView *toView = [[transitionContext viewControllerForKey:UITransitionContextToViewControllerKey] view];
     UIView *containerView = [transitionContext containerView];  //  转场中间视图
     
-//    [containerView addSubview:fromView];
+    UIView *bg = [[UIView alloc] init];
+    bg.backgroundColor = [UIColor blackColor];
+    bg.frame = fromView.bounds;
+    [containerView addSubview:bg];
+    
+    toView.frame = [[UIScreen mainScreen] bounds];
+    
     [containerView addSubview:toView];
     [containerView addSubview:self.view];
-    
     toView.hidden = YES;
-    
     [UIView animateWithDuration:[self animationDuration] animations:^{
         
+        CGFloat w0 = self.view.frame.size.width;
+        CGFloat h0 = self.view.frame.size.height;
+        CGFloat w1, h1;
+        
+        if (w0 > h0) {
+            w1 = [[UIScreen mainScreen] bounds].size.width;
+            h1 = h0/w0 * w1;
+        } else {
+            h1 = [[UIScreen mainScreen] bounds].size.height;
+            w1 = w0/h0 * h1;
+        }
+        
+        self.view.bounds = CGRectMake(0, 0, w1, h1);
         self.view.center = self.center;
         
     } completion:^(BOOL finished) {
